21fe84f2291e2d3a098a368cad7c42a432798e89
DevSwayam <swayamkarle2@gmail.com>
2023-07-09 00:29:44 +0530

    Header Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,18 @@
-
 import {Routes, BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
-
-
 const App = () => {
   return (
-    <>
-    
-     <Router>   
-        <div className='container'>
-          <Routes> 
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='*' element={<NotFound />} />{/* Any Page that is not available will get redirected to not found using (*) */}
-          </Routes>
-        </div>
-      </Router>
+    <>  
+        <Router>   
+          <div>
+            <Routes> 
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='*' element={<NotFound />} />{/* Any Page that is not available will get redirected to not found using (*) */}
+            </Routes>
+          </div>
+        </Router>
     </>
   );
 
