d014c59664d46c6dd16fe27925ff61a64ac5ae53
DevSwayam <swayamkarle2@gmail.com>
2023-07-14 01:48:33 +0530

    Routes Added and some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
-// import NavBar from './components/NavBar';
+import HomeCards from './components/HomeCards'
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-
-      
-        {/* <NavBar className='w-screen h-auto' /> */}
+        {/*  */}
         <Router>
+        <NavBar className='w-screen h-auto' />
           <Routes>
-
+            <Route exact path='/resources' element={<HomeCards/>}></Route>
             <Route exact path='/' element={<Home />} />
             <Route exact path='*' element={<NotFound />} />{/* Any Page that is not available will get redirected to not found using (*) */}
           </Routes>
+        <Footer/>
         </Router>
      
     </>
