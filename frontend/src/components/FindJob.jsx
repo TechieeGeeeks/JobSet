b19b9dc770e810ce3b7875466b945bd83eb8d294
DevSwayam <swayamkarle2@gmail.com>
2023-07-14 16:37:55 +0530

    Configured Route for Find Job and added a contact us component + Added find job component

---
+++ frontend/src/components/FindJob.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const FindJob = () => {
+    return (
+        <>
+        <div className="container mx-auto py-8 ">
+          <h1 className="text-4xl font-bold mb-6 mt-4">My Component</h1>
+          <p className="text-lg text-gray-800 mb-4">This is a sample Job listing with Tailwind CSS styling.</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Click Me
+          </button>
+        </div>
+        <div className="container mx-auto py-8 ">
+          <h1 className="text-4xl font-bold mb-6 mt-4">My Component</h1>
+          <p className="text-lg text-gray-800 mb-4">This is a sample Job listing with Tailwind CSS styling.</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Click Me
+          </button>
+        </div>
+
+        <div className="container mx-auto py-8 ">
+          <h1 className="text-4xl font-bold mb-6 mt-4">My Component</h1>
+          <p className="text-lg text-gray-800 mb-4">This is a sample Job listing with Tailwind CSS styling.</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Click Me
+          </button>
+        </div><div className="container mx-auto py-8 ">
+          <h1 className="text-4xl font-bold mb-6 mt-4">My Component</h1>
+          <p className="text-lg text-gray-800 mb-4">This is a sample Job listing with Tailwind CSS styling.</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Click Me
+          </button>
+        </div>
+        
+        </>
+        
+      );
+}
+
+export default FindJob
