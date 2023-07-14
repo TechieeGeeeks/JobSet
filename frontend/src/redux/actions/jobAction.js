5415f3f8a5d76152aaab20ef56fafb0fd9bc7ae9
DevSwayam <swayamkarle2@gmail.com>
2023-07-15 02:43:47 +0530

    Job Actions Api prerequisites

--- frontend/src/redux/actions/jobAction.js
+++ frontend/src/redux/actions/jobAction.js
@@ -1,12 +1,19 @@
-import { JOB_LOAD_REQUEST } from "../constants/jobConstant"
+import { JOB_LOAD_FAIL, JOB_LOAD_REQUEST, JOB_LOAD_SUCCESS } from "../constants/jobConstant"
 import axios from 'axios';
 
 export const jobLoadAction =(pageNumber, keyword='', cat='', location='')=>async(dispatch)=>{
     dispatch({type: JOB_LOAD_REQUEST});
     try {
-        const {data} = await axios.get(`/api/jobs?pageNumber=${pageNumber}&keyword${keyword}=&cat${cat}=&location=${location}`)
+        const {data} = await axios.get(`/api/jobs?pageNumber=${pageNumber}&keyword${keyword}=&cat${cat}=&location=${location}`);
+        dispatch({
+            type: JOB_LOAD_SUCCESS,
+            payload: data
+        })
     } catch (error) {
-        
+        dispatch({
+            type: JOB_LOAD_FAIL,
+            payload: error.response.data.error
+        });
     }
 
 }
