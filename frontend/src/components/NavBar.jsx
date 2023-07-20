62d355a9e66d1432ed3b569a91142557dbd00e7c
DevSwayam <swayamkarle2@gmail.com>
2023-07-21 00:58:29 +0530

    Conflict Resolving Commit

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -9,14 +9,15 @@ import UserDashboard from "../img/user_dashboard.png"
 import AdminDashboard from "../img/admin_dashboard.png"
 import Logo from "../img/shirt-svgrepo-com.svg"
 import PrivacyPolicyIcon from "../img/privacy_policy.png"
-
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { useEffect } from "react";
 
 const NavBar = () => {
     const [isMenu, setIsMenu] = useState(false);
-    const isLoggedIn = true;
+    const { userInfo } = useSelector((state) => state.signIn);
     const [selectedItem, setSelectedItem] = useState(null);
-
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const handleItemClick = (index) => {
         setSelectedItem(index);
     };
@@ -25,6 +26,12 @@ const NavBar = () => {
     const dropDownHandle = () => {
         setIsMenu(!isMenu);
     }
+
+    useEffect(()=>{
+        if(userInfo){
+            setIsLoggedIn(true);
+        }
+    },[userInfo])
 
 
     return (
@@ -93,12 +100,15 @@ const NavBar = () => {
                                 </>
                             ) : (
                                 <>
+                                <Link to= '/login'>
                                     <li className="text-lg text-lightModeTextColor hover:text-lightPrimary duration-100 transition-all ease-in-out cursor-pointer">
                                         Login
                                     </li>
+                                </Link>
                                     <li className="rounded-full border border-lightPrimary py-3 px-9 text-lg text-lightModeTextColor hover:shadow-lg duration-100 transition-all ease-in-out cursor-pointer">
                                         Register Now
                                     </li>
+                                
                                 </>
                             )}
                         </ul>
@@ -143,6 +153,7 @@ const NavBar = () => {
                                             Settings
                                         </p>
                                     </div>
+                                    
                                     <Link to='/policy'>
                                         <div className="flex px-4 py-2 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-lightModeTextColor">
                                             <img src={PrivacyPolicyIcon} className="pl-1" alt="logout_icon" />
@@ -153,15 +164,17 @@ const NavBar = () => {
                                             </p>
                                         </div>
                                     </Link>
-
                                     <div className="flex px-4 py-2 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-lightModeTextColor">
                                         <img src={LogOutIcon} className="pl-1" alt="logout_icon" />
+                                        <Link to='/' onClick={()=>{setIsLoggedIn(false)}}> 
                                         <p
                                             className="w-full flex items-center justify-center gap-3 text-red-600"
                                         >
                                             Log out
                                         </p>
+                                        </Link>
                                     </div>
+                                   
                                 </div>
                             )}
                         </div>
@@ -180,7 +193,7 @@ const NavBar = () => {
                     </div>
 
                     <div className="relative">
-                        {isLoggedIn ? (
+                        {isLoggedIn? (
                             <img
                                 // whileTap={{ scale: 0.6 }}
                                 src={ProfileImage}
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,15 +3,21 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import { loadJobReducer } from './reducers/jobReducer';
 import { loadJobTypeReducer } from './reducers/jobTypeReducer';
+import { userReducerSignIn } from './reducers/userReducer';
 
 // Combine Reducers
 const reducer = combineReducers({
     loadJobs: loadJobReducer,
-    jobTypeAll: loadJobTypeReducer
+    jobTypeAll: loadJobTypeReducer,
+    signIn: userReducerSignIn
 });
 
 // Intital State
-let initialState = {};
+let initialState = {
+   signIn:{
+    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')):null
+   }
+};
 const middleware = [thunk];
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
