70efec2e8b4671d15d845fb3d5926623b3538303
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-07-20 10:06:34 -0700

    Modified the policy component

--- frontend/src/components/Policy.jsx
+++ frontend/src/components/Policy.jsx
@@ -7,7 +7,7 @@ const Policy = () => {
       <div className="col-span-1">
         <div className="p-8 max-w-2xl mt-24">
           <div className="border-solid border mb-4 p-4 ">
-            <h2 className="text-xl font-bold border-b-2 pb-2">Privacy Policy</h2>
+            <h2 className="text-lg font-bold border-b-2 pb-2">Privacy Policy</h2>
             <p className="mb-4">
               At [Your Company Name], we take your privacy seriously. This Privacy
               Policy outlines how we collect, use, and protect the personal
@@ -21,19 +21,19 @@ const Policy = () => {
       <div className="col-span-1 mt-24">
         <div className="p-8 max-w-2xl">
           <div className="border-solid border mb-4 p-4">
-            <h2 className="text-xl font-bold border-b-2 pb-2">Information Collection</h2>
+            <h2 className="text-lg font-bold border-b-2 pb-2">Information Collection</h2>
             <p className="mb-4">
               We collect personal information such as your name, email address, and
               contact details when you voluntarily submit them to us through our
               website forms or other means of communication.
             </p>
-            <p>
+            <p className="mb-4">
               The personal information we collect is used to provide you with the
               services and information you request. We may also use this information
               to improve our website, customize your user experience, and send you
               relevant updates or promotional materials.
             </p>
-            <h3 className="text-xl font-bold mb-2">Information Protection</h3>
+            <h3 className="text-lg font-bold mb-2">Information Protection</h3>
             <p className="mb-4">
               We implement strict security measures to safeguard your personal
               information from unauthorized access, disclosure, or alteration. We do
@@ -41,7 +41,7 @@ const Policy = () => {
               with your explicit consent.
             </p>
 
-            <h3 className="text-xl font-bold mb-2">Your Choices</h3>
+            <h3 className="text-lg font-bold mb-2">Your Choices</h3>
             <p className="mb-4">
               You have the right to access, update, or delete the personal information
               we hold about you. If you wish to exercise any of these rights or have
