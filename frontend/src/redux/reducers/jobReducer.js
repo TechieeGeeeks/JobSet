ebb6265cc561a869a665b88b65a2c6f134fbe948
DevSwayam <swayamkarle2@gmail.com>
2023-07-26 01:50:50 +0530

    User can view every single job

--- frontend/src/redux/reducers/jobReducer.js
+++ frontend/src/redux/reducers/jobReducer.js
@@ -1,4 +1,11 @@
-import { JOB_LOAD_FAIL, JOB_LOAD_REQUEST, JOB_LOAD_RESET, JOB_LOAD_SUCCESS } from "../constants/jobConstant"
+import {  JOB_LOAD_FAIL,
+    JOB_LOAD_REQUEST,
+    JOB_LOAD_RESET,
+    JOB_LOAD_SINGLE_FAIL,
+    JOB_LOAD_SINGLE_REQUEST,
+    JOB_LOAD_SINGLE_RESET,
+    JOB_LOAD_SINGLE_SUCCESS,
+    JOB_LOAD_SUCCESS } from "../constants/jobConstant"
 
 export const loadJobReducer = (state={jobs:[]},action)=>{
     switch(action.type){
@@ -24,4 +31,27 @@ export const loadJobReducer = (state={jobs:[]},action)=>{
             default:
                 return state;
     }
+}
+
+// single job reducer
+export const loadJobSingleReducer = (state = { job: {} }, action) => {
+    switch (action.type) {
+        case JOB_LOAD_SINGLE_REQUEST:
+            return { loading: true }
+        case JOB_LOAD_SINGLE_SUCCESS:
+            return {
+
+                loading: false,
+                success: action.payload.success,
+                singleJob: action.payload.job,
+
+            }
+        case JOB_LOAD_SINGLE_FAIL:
+            return { loading: false, error: action.payload }
+        case JOB_LOAD_SINGLE_RESET:
+            return {}
+        default:
+            return state;
+    }
+
 }
