ddcae949cfa7cec247477b9e2f8821667a59fb80
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-07-27 14:12:13 +0530

    Changing Demo

--- frontend/src/components/Demo.jsx
+++ frontend/src/components/Demo.jsx
@@ -68,8 +68,10 @@ const Demo = () => {
 
   return (
     <section>
-      <div className='h-screen'>
-        <div className='pt-40 p-16'>
+
+      {/* For Desktops */}
+      <div className='hidden md:flex h-screen w-screen items-center justify-center'>
+        <div className='md:pt-60 lg:pt-40 p-16'>
           <div className='flex items-center justify-center gap-6'>
             <div className='w-1/2 p-4 bg-lightCard shadow-xl rounded-full flex items-center justify-center'>
               <input type="text" placeholder='Job Title' className='w-1/2 h-full px-12 rounded-full outline-none' />
@@ -173,6 +175,20 @@ const Demo = () => {
 
         </div>
       </div>
+
+      {/* For Mobile */}
+      <div className='md:hidden flex flex-col w-screen h-screen pt-32'>
+      <div className='flex flex-col items-center justify-center gap-6 px-8'>
+            <div className='p-4 bg-lightCard shadow-xl rounded-full flex items-center justify-center'>
+              <input type="text" placeholder='Job Title' className='w-1/2 h-full px-8 rounded-full outline-none' />
+              <input type="text" placeholder='Location' className='w-1/2 h-full px-8 rounded-e-full border-l-2 outline-none' />
+            </div>
+            <button className='bg-lightPrimary text-lightCard p-4 px-12 rounded-full'>Search</button>
+          </div>
+      </div>
+
+
+    
     </section>
   )
 }
