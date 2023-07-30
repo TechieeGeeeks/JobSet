5f7e008e4300ac41642b5f31273104a4dad5e885
Swayam <swayamkarle_ce_2020@ltce.in>
2023-07-30 20:13:35 +0530

    ALL COMPONENTS UI IS MADE

--- frontend/src/pages/global/HeaderTop.jsx
+++ frontend/src/pages/global/HeaderTop.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { useState } from 'react';
-import { useProSidebar } from 'react-pro-sidebar';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useProSidebar } from "react-pro-sidebar";
+import MenuIcon from "@mui/icons-material/Menu";
+
 
 const HeaderTop = () => {
+  const navigate = useNavigate();
   const { collapseSidebar } = useProSidebar();
+  // Handle back button click
+  const handleGoBack = () => {
+    navigate(-1); // Go back to the previous page
+  };
 
   return (
     <div className="flex-grow">
@@ -20,18 +25,28 @@ const HeaderTop = () => {
                 <MenuIcon />
               </button>
               <span className="ml-2 text-xl font-bold">HR APP</span>
+              
+
             </div>
             <div className="hidden sm:flex sm:items-center sm:ml-6">
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-
+                 
                 </div>
-
               </div>
             </div>
           </div>
+          
         </div>
       </nav>
+      {
+      /*<button
+          className=" bg-[#0d1bcf] text-white px-4 py-2 rounded-lg 	"
+          onClick={handleGoBack}
+        >
+          Back
+        </button> */
+    }
     </div>
   );
 };
